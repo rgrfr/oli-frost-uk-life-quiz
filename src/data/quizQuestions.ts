8ac0bebe9dd9c995a5d2d57a852f357854309ae5
rgr <13759767+rgrfr@users.noreply.github.com>
2025-06-01 13:00:49 +0100

    order of sections

--- src/data/quizQuestions.ts
+++ src/data/quizQuestions.ts
@@ -18,8 +18,8 @@ export const quizSections = [
   "culture & customs",
   "geography & places", 
   "language & social",
-  "shops & commerce",
   "nature"
+  "shops & commerce",
 ];
 
 export const questions: Question[] = [
